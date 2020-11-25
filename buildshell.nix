a81db5731566cac15908f50087502552fffc7152
codygman <cody@itpro.tv>
2020-11-25 10:43:16 -0600

    update nixpkgs; use new shell.inputDerivation
    https://nixos.wiki/wiki/Caching_nix_shell_build_inputs#inputDerivation

--- buildshell.nix
+++ buildshell.nix
@@ -1,3 +1,3 @@
 let shell = import ./shell.nix {};
     pkgs = import ./nixpkgs.nix {};
-in shell.buildInputs ++ [ pkgs.stdenv pkgs.haskell.compiler.ghc8101 ]
+in shell.inputDerivation
--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-import (fetchTarball "https://github.com/tweag/nixpkgs/archive/331f0ec32598e8a0984d9d5200f85535d28d7cbb.tar.gz")
+import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/6625284c397b44bc9518a5a1567c1b5aae455c08.tar.gz")
